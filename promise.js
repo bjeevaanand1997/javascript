3d8b0580449ea840eb37728dd1fe460b54873504
bjeevaanand1997 <bjeevaanand@gmail.com>
2025-10-04 15:37:52 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -2,4 +2,5 @@ console.log("start the script");
 console.log("start the scipt");
 console.log("start the scrip");
 console.log("start the sript");
-
+console.log("jeeva made the change");
+Console.log("in javascript");
